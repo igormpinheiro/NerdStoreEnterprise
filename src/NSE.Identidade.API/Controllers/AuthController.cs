a13286a21546599da09479c8c30c48caa87b9b6d
Igor Martins Pinheiro <igordf@gmail.com>
2022-12-01 17:03:32 -0300

    feat: Implementa a controladora de autenticação

---
+++ src/NSE.Identidade.API/Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Identidade.API.DTOs;
+
+namespace NSE.Identidade.API.Controllers;
+
+[Route("api/identidade")]
+public class AuthController : Controller
+{
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+    {
+        _signInManager = signInManager;
+        _userManager = userManager;
+    }
+
+    [HttpPost("Autenticar")]
+    public async Task<IActionResult> Login(UsuarioLoginDTO usuarioLogin)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest();
+
+        var result = await _signInManager.PasswordSignInAsync(usuarioLogin.Email, usuarioLogin.Senha,
+            false,
+            true);
+
+        if (result.Succeeded)
+            return Ok();
+
+        return BadRequest();
+    }
+
+    [HttpPost("nova-conta")]
+    public async Task<ActionResult> Registrar(UsuarioRegistroDTO usuarioRegistro)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest();
+
+        var user = new IdentityUser()
+        {
+            UserName = usuarioRegistro.Email,
+            Email = usuarioRegistro.Email,
+            EmailConfirmed = true
+        };
+
+        var result = await _userManager.CreateAsync(user, usuarioRegistro.Senha);
+
+        if (result.Succeeded)
+        {
+            await _signInManager.SignInAsync(user, false);
+            return Ok();
+        }
+
+        return BadRequest(result.Errors);
+    }
+}
---
+++ src/NSE.Identidade.API/DTOs/UsuarioRegistroDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NSE.Identidade.API.DTOs;
+
+public class UsuarioRegistroDTO
+{
+    public UsuarioRegistroDTO(string email, string senha, string senhaConfirmacao)
+    {
+        Email = email;
+        Senha = senha;
+        SenhaConfirmacao = senhaConfirmacao;
+    }
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [EmailAddress(ErrorMessage = "O campo {0} está em um formato inválido")] 
+    public string Email { get; set; }
+    
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+    public string Senha { get; set; }
+    
+    [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+    [Compare("Senha", ErrorMessage = "As senhas não conferem.")]
+    public string SenhaConfirmacao { get; set; }
+}
