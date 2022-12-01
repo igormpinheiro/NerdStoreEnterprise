9ab93a6ffbbe1329f5bb1ddad37aacaf58ba6e4d
Igor Martins Pinheiro <igordf@gmail.com>
2022-11-30 22:32:10 -0300

    Primeiro commit

---
+++ src/NSE.Identidade.API/Configuration/ResolveDependency.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.Identidade.API.Data;
+
+namespace NSE.Identidade.API.Configuration;
+
+public class ResolveDependency
+{
+    
+    //Configure DBContext
+    public static void ConfigureDbContext(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        });
+    }
+    
+    public static void ConfigureServices(IServiceCollection services)
+    {
+        // // Infra - Data
+        // services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+        // services.AddScoped<IUnitOfWork, UnitOfWork>();
+        // services.AddScoped<IdentidadeContext>();
+        //
+        // // Infra - Identity
+        // services.AddScoped<IAspNetUser, AspNetUser>();
+        //
+        // // Services
+        // services.AddScoped<IUsuarioService, UsuarioService>();
+    }
+    
+}
---
+++ src/NSE.Identidade.API/Program.cs
@@ -0,0 +1,31 @@
+using NSE.Identidade.API.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Resolve Dependecies 
+ResolveDependency.ConfigureDbContext(builder.Services, builder.Configuration);
+ResolveDependency.ConfigureServices(builder.Services);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
