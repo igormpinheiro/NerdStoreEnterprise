d5989ba8556b75a4ad12b5c64a5453c3c7b43e66
Igor Martins Pinheiro <igordf@gmail.com>
2022-12-01 22:17:47 -0300

    Feat: Implementa emissão do JWT pela controller

---
+++ src/NSE.Identidade.API/DTOs/UsuarioLoginRespostaDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace NSE.Identidade.API.DTOs;
+
+public class UsuarioLoginRespostaDTO
+{
+    public string AccessToken { get; set; }
+    public double ExpiresIn { get; set; }
+    public UsuarioTokenDTO UsuarioToken { get; set; }
+}
+
+public class UsuarioTokenDTO
+{
+    public string Id { get; set; }
+    public string Email { get; set; }
+    public IEnumerable<UsuarioClaimDTO> Claims { get; set; }
+}
+
+public class UsuarioClaimDTO
+{
+    public string Value { get; set; }
+    public string Type { get; set; }
+}
