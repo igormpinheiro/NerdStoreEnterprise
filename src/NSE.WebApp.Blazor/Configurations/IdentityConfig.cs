66e7d8ff36a8f268ef74eaa02b047cd5cbb39871
Igor Martins Pinheiro <igordf@gmail.com>
2022-12-02 22:48:07 -0300

    feat: adiciona projetos MVC e Blazor ao projeto

---
+++ src/NSE.WebApp.Blazor/Configurations/IdentityConfig.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace NSE.WebApp.Blazor.Configurations;
+
+public static class IdentityConfig
+{
+    public static void AddIdentityConfiguration(this IServiceCollection services)
+    {
+        services.AddOptions();
+        services.AddAuthorizationCore();
+        
+    }
+    
+    public static void UseIdentityConfiguration(this WebAssemblyHostBuilder app)
+    {
+        // app.UseAuthentication();
+        // app.UseAuthorization();
+    }
+    
+}
---
+++ src/NSE.WebApp.MVC/Configurations/IdentityConfig.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace NSE.WebApp.MVC.Configurations;
+
+public static class IdentityConfig
+{
+    public static void AddIdentityConfiguration(this IServiceCollection services)
+    {
+        services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options =>
+            {
+                options.LoginPath = "/login";
+                options.AccessDeniedPath = "/acesso-negado";
+            });
+        
+        // if (services == null) throw new ArgumentNullException(nameof(services));
+        //
+        // services.AddIdentity<IdentityUser, IdentityRole>()
+        //     .AddEntityFrameworkStores<ApplicationDbContext>()
+        //     .AddDefaultTokenProviders();
+        //
+        // // Cookie
+        // services.ConfigureApplicationCookie(options =>
+        // {
+        //     options.LoginPath = "/login";
+        //     options.LogoutPath = "/logout";
+        //     options.AccessDeniedPath = "/acesso-negado";
+        //     options.SlidingExpiration = true;
+        //     options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+        //     options.Cookie = new CookieBuilder
+        //     {
+        //         HttpOnly = true,
+        //         Name = ".AspNetCore.Identity.Application",
+        //         SameSite = SameSiteMode.Strict
+        //     };
+        // });
+    }
+    
+    public static void UseIdentityConfiguration(this IApplicationBuilder app)
+    {
+        app.UseAuthentication();
+        app.UseAuthorization();
+    }
+    
+}
